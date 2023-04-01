69f18e6410277b1d9ec02b8db7095b42f795a733
ricky-kiva <ricky.kiva@gmail.com>
2023-04-01 14:13:18 +0700

    complete News Fragment

--- app/src/main/java/com/dicoding/newsapp/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/dicoding/newsapp/data/remote/retrofit/ApiService.kt
@@ -7,6 +7,6 @@ import retrofit2.http.Query
 
 interface ApiService {
     // this is what's being called: https://newsapi.org/v2/top-headlines?country=id&category=science&apiKey=apiKey
-    @GET("top-headlines?country=id&category=science")
+    @GET("top-headlines?country=us&category=science")
     fun getNews(@Query("apiKey") apiKey: String): Call<NewsResponse>
 }
